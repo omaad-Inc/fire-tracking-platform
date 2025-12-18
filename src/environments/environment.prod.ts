774ee8e658533e16e9bc7a6651b937b82db3fb2f
Mbaye SENE <mbaye.sene@manomano.com>
2025-12-18 11:05:02 +0100

    feat: improve pwa update detection ..

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    //apiUrl: 'https://fire-tracking-backend.vercel.app/api/v1',
-    apiUrl: 'http://localhost:8000/api/v1',
+    apiUrl: 'https://fire-tracking-backend.vercel.app/api/v1',
+    //apiUrl: 'http://localhost:8000/api/v1',
     googleClientId: '' // Set this from Google Cloud Console
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
     // For local development with deployed backend:
-    //apiUrl: 'https://fire-tracking-backend.vercel.app/api/v1',
-    apiUrl: 'http://localhost:8000/api/v1',
+    apiUrl: 'https://fire-tracking-backend.vercel.app/api/v1',
+    //apiUrl: 'http://localhost:8000/api/v1',
     googleClientId: '' // Set this from Google Cloud Console
 };
 
