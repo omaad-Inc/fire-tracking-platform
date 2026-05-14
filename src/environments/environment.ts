b8a4553968085907d800450a32717b3ccd487299
Mbaye SENE <mbaye.sene@manomano.com>
2026-05-14 11:40:38 +0200

    adding wealth score widget logic and updating add assets cards..

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    //apiUrl: 'https://fire-tracking-backend.vercel.app/api/v1',
-    apiUrl: 'http://localhost:8000/api/v1',
+    apiUrl: 'https://fire-tracking-backend.vercel.app/api/v1',
+    //apiUrl: 'http://localhost:8000/api/v1',
     googleClientId: '338569032785-oosvdq7uk3jfhqtmqap4565dncceahud.apps.googleusercontent.com'
 };
 
